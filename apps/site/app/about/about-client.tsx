91f84da097ad618ded72ed640786f4d2e77cbde4
John Schibelli <jschibelli@gmail.com>
2025-10-04 17:03:58 -0400

    fix: final cleanup of about page client component

--- apps/site/app/about/about-client.tsx
+++ apps/site/app/about/about-client.tsx
@@ -105,6 +105,7 @@ export function AboutPageClient() {
         <div className="absolute inset-0 opacity-10">
           <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23ffffff" fill-opacity="0.1"%3E%3Ccircle cx="30" cy="30" r="2"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] bg-repeat" />
         </div>
+        </div>
 
         <Container className="relative z-10 px-4">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center min-h-[80vh]">
